9593fb5817c60adcac7c09b93ad6d5808d089c51
Lertyj <alexroleplay2019@gmail.com>
2024-01-07 04:14:24 +0300

    Creating trending bids
    The Trending bids component was created, Header and Sidebar were also adjusted

--- src/Content/Home/Home.js
+++ src/Content/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Discover from "./Discover/Discover";
 import TopNFT from "./TopNFT/TopNFT";
 import styles from './Home.module.css'
+import TrendingBids from "./NFT-auction/NFT-auction";
 function Home(){
     return(
         <>
         <div className={styles.cards}>
             <Discover/>
             <TopNFT/>
+        </div>
+        <div className={styles.trendingBids}>
+            <TrendingBids/>
         </div>
         
         </>
