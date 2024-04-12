990c4be426004322ec19b50b08728d8b86df0a33
Lertyj <alexroleplay2019@gmail.com>
2024-04-13 00:27:05 +0300

    Fixed issue 19
    The Active bids component has been redesigned and is now a table rather than div elements

--- src/Content/Home/Active_Bids/Active_Bids.jsx
+++ src/Content/Home/Active_Bids/Active_Bids.jsx
@@ -8,32 +8,32 @@ import nft4 from "../../../icons/nft.jpg";
 import avatar from "../../../icons/avatar1.svg";
 export function BidTitle() {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.checkbox}>
+    <tr className={`${classes.wrapper} ${classes.bids_title}`}>
+      <th className={classes.checkbox}>
         <label className={classes.label}>
           <input type="checkbox" className={classes.real_checkbox_title} />
           <span className={classes.custom_checkbox_title}></span>
         </label>
-      </div>
-      <div className={classes.item}>
+      </th>
+      <th className={classes.item_bid}>
         <h5>Item List</h5>
-      </div>
-      <div className={classes.open_price}>
+      </th>
+      <th className={classes.open_price}>
         <h5>Open Price</h5>
-      </div>
-      <div className={classes.your_offer}>
+      </th>
+      <th className={classes.your_offer}>
         <h5>Your Offer</h5>
-      </div>
-      <div className={classes.recent_offer}>
+      </th>
+      <th className={classes.recent_offer}>
         <h5>Recent Offer</h5>
-      </div>
-      <div className={classes.time}>
+      </th>
+      <th className={classes.time}>
         <h5>Time Left</h5>
-      </div>
-      <div className={classes.delete}>
+      </th>
+      <th className={classes.delete}>
         <h5>Action</h5>
-      </div>
-    </div>
+      </th>
+    </tr>
   );
 }
 export function ActiveBid({
@@ -49,41 +49,41 @@ export function ActiveBid({
   time,
 }) {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.checkbox}>
+    <tr className={`${classes.wrapper} ${classes.item}`}>
+      <td className={classes.checkbox}>
         <button>
           <label className={classes.label}>
             <input type="checkbox" className={classes.real_checkbox} />
             <span className={classes.custom_checkbox}></span>
           </label>
         </button>
-      </div>
-      <div className={classes.item}>
+      </td>
+      <td className={classes.item_bid}>
         <img src={nft} alt={nft_alt} />
         <div className={classes.item_information}>
           <h4>{nft_name}</h4>
           <p>{nft_owner}</p>
         </div>
-      </div>
-      <div className={classes.open_price}>
+      </td>
+      <td className={classes.open_price}>
         <p>{open_price}</p>
-      </div>
-      <div className={classes.your_offer}>
+      </td>
+      <td className={classes.your_offer}>
         <p>{your_offer}</p>
-      </div>
-      <div className={classes.recent_offer}>
+      </td>
+      <td className={classes.recent_offer}>
         <img src={avatar} alt={avatar_alt} />
-        <div className={classes.recent_information}>
+        <td className={classes.recent_information}>
           <p>{recent_offer}</p>
-        </div>
-      </div>
-      <div className={classes.time}>
+        </td>
+      </td>
+      <td className={classes.time}>
         <p>{time}</p>
-      </div>
+      </td>
       <button className={classes.delete}>
         <span className={classes.cross}></span>
       </button>
-    </div>
+    </tr>
   );
 }
 
@@ -94,71 +94,59 @@ function ActiveBids() {
         <h3>Active Bids</h3>
       </div>
       <div className={styles.content}>
-        <div className={styles.bid_title}>
+        <table className={styles.table_wrapper}>
           <BidTitle />
-        </div>
-        <ul>
-          <li className={styles.item}>
-            <ActiveBid
-              nft={nft1}
-              nft_name="Cute Cube Cool"
-              nft_owner="John Abraham"
-              open_price="0.0025 ETH"
-              your_offer="0.0025 ETH"
-              avatar={avatar}
-              recent_offer="0.0025 ETH"
-              time="2 Hours 1 min 30s"
-            />
-          </li>
-          <li className={styles.item}>
-            <ActiveBid
-              nft={nft2}
-              nft_name="Liquid Wave"
-              nft_owner="John Abraham"
-              open_price="0.0025 ETH"
-              your_offer="0.0025 ETH"
-              avatar={avatar}
-              recent_offer="0.0025 ETH"
-              time="2 Hours 1 min 30s"
-            />
-          </li>
-          <li className={styles.item}>
-            <ActiveBid
-              nft={nft1}
-              nft_name="Cute Cube Cool"
-              nft_owner="John Abraham"
-              open_price="0.0025 ETH"
-              your_offer="0.0025 ETH"
-              avatar={avatar}
-              recent_offer="0.0025 ETH"
-              time="2 Hours 1 min 30s"
-            />
-          </li>
-          <li className={styles.item}>
-            <ActiveBid
-              nft={nft3}
-              nft_name="Liquid Wave"
-              nft_owner="John Abraham"
-              open_price="0.0025 ETH"
-              your_offer="0.0025 ETH"
-              avatar={avatar}
-              recent_offer="0.0025 ETH"
-              time="2 Hours 1 min 30s"
-            />
-          </li>
-          <li className={`${styles.item} ${styles.last}`}>
-            <ActiveBid
-              nft={nft4}
-              nft_name="Liquid Wave"
-              nft_owner="John Abraham"
-              open_price="0.0025 ETH"
-              your_offer="0.0025 ETH"
-              avatar={avatar}
-              recent_offer="0.0025 ETH"
-              time="2 Hours 1 min 30s"
-            />
-          </li>
-        </ul>
+          <ActiveBid
+            nft={nft1}
+            nft_name="Cute Cube Cool"
+            nft_owner="John Abraham"
+            open_price="0.0025 ETH"
+            your_offer="0.0025 ETH"
+            avatar={avatar}
+            recent_offer="0.0025 ETH"
+            time="2 Hours 1 min 30s"
+          />
+          <ActiveBid
+            nft={nft2}
+            nft_name="Liquid Wave"
+            nft_owner="John Abraham"
+            open_price="0.0025 ETH"
+            your_offer="0.0025 ETH"
+            avatar={avatar}
+            recent_offer="0.0025 ETH"
+            time="2 Hours 1 min 30s"
+          />
+          <ActiveBid
+            nft={nft1}
+            nft_name="Cute Cube Cool"
+            nft_owner="John Abraham"
+            open_price="0.0025 ETH"
+            your_offer="0.0025 ETH"
+            avatar={avatar}
+            recent_offer="0.0025 ETH"
+            time="2 Hours 1 min 30s"
+          />
+          <ActiveBid
+            nft={nft3}
+            nft_name="Liquid Wave"
+            nft_owner="John Abraham"
+            open_price="0.0025 ETH"
+            your_offer="0.0025 ETH"
+            avatar={avatar}
+            recent_offer="0.0025 ETH"
+            time="2 Hours 1 min 30s"
+          />
+          <ActiveBid
+            nft={nft4}
+            nft_name="Liquid Wave"
+            nft_owner="John Abraham"
+            open_price="0.0025 ETH"
+            your_offer="0.0025 ETH"
+            avatar={avatar}
+            recent_offer="0.0025 ETH"
+            time="2 Hours 1 min 30s"
+          />
+        </table>
       </div>
     </div>
   );
