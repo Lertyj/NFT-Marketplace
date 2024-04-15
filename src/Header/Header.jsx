e98a2e5893b14fabf1c42f7ad02f676cb9778d85
Lertyj <alexroleplay2019@gmail.com>
2024-04-15 19:38:36 +0300

    FIxed issue 9
    Now you can easily add new themes and icons to them on the page. The theme is saved when the page is reloaded

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./Header.module.css";
 import { ReactComponent as SearchImage } from "../icons/searchimage.svg";
 import ProfileImage from "../icons/profile-icon.svg";
 import { ReactComponent as BellImage } from "../icons/bellimage.svg";
-import DarkModeImage from "../icons/darkmode.svg";
-import LightModeImage from "../icons/lightmode.svg";
+import ThemeManager from "../ThemeManager/ThemeManager";
 
 function Header() {
-  const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") || "dark";
-  });
-
-  const ToggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-  };
-
-  useEffect(() => {
-    const container = document.querySelector(".App_app_container__V-0Go");
-    if (container) {
-      container.setAttribute("data-theme", theme);
-    }
-  }, [theme]);
-
   return (
     <div className={styles.header}>
       <div className={styles.header_content}>
@@ -45,16 +27,7 @@ function Header() {
           </form>
         </div>
         <div className={styles.buttons}>
-          <button
-            onClick={() => {
-              ToggleTheme();
-            }}
-          >
-            <img
-              src={theme === "light" ? DarkModeImage : LightModeImage}
-              alt="theme button"
-            />
-          </button>
+          <ThemeManager />
           <button className={styles.bell}>
             <BellImage className="svg" />
           </button>
--- src/ThemeManager/ThemeManager.jsx
+++ src/ThemeManager/ThemeManager.jsx
@@ -1,17 +1,51 @@
-import { useState, useEffect } from "react";
-const [theme, setTheme] = useState(() => {
-  return localStorage.getItem("theme") || "dark";
-});
+import React, { useState, useEffect } from "react";
+import { ReactComponent as DarkModeImage } from "../icons/darkmode.svg";
+import { ReactComponent as LightModeImage } from "../icons/lightmode.svg";
+import { ReactComponent as MoonSunImage } from "../icons/lightdarkmode.svg";
+import styles from "./ThemeManager.module.css";
+const ThemeManager = () => {
+  const themes = {
+    dark: <DarkModeImage className={styles.theme_svg} />,
+    light: <LightModeImage className={styles.theme_svg} />,
+    lightdark: <MoonSunImage className={styles.theme_svg} />,
+  };
 
-export const ToggleTheme = () => {
-  const newTheme = theme === "light" ? "dark" : "light";
-  setTheme(newTheme);
-  localStorage.setItem("theme", newTheme);
+  const [selectedTheme, setSelectedTheme] = useState(
+    localStorage.getItem("selectedTheme") || "dark"
+  );
+  const [showThemes, setShowThemes] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("selectedTheme", selectedTheme);
+    document
+      .querySelector(".App_app_container__V-0Go")
+      .setAttribute("data-theme", selectedTheme);
+  }, [selectedTheme]);
+
+  const changeTheme = (theme) => {
+    setSelectedTheme(theme);
+    setShowThemes(false);
+  };
+
+  const getThemeIcon = () => {
+    return themes[selectedTheme];
+  };
+
+  return (
+    <div className={styles.theme_manager}>
+      <button onClick={() => setShowThemes(!showThemes)}>
+        {getThemeIcon()}
+      </button>
+      {showThemes && (
+        <div className={styles.theme_list}>
+          <button onClick={() => changeTheme("dark")}>{themes.dark}</button>
+          <button onClick={() => changeTheme("light")}>{themes.light}</button>
+          <button onClick={() => changeTheme("lightdark")}>
+            {themes.lightdark}
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
-
-useEffect(() => {
-  const container = document.querySelector(".App_app_container__V-0Go");
-  if (container) {
-    container.setAttribute("data-theme", theme);
-  }
-}, [theme]);
+export default ThemeManager;
